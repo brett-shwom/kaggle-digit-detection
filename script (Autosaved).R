06441082a9800827987ffd4ad33ad3b47d4e1a7d
Brett Shwom <brett.shwom@nyu.edu>
2015-03-21 01:42:29 -0400

    removing autosaved script

--- script (Autosaved).R
+++
@@ -1,5 +0,0 @@
-function sumMatrixRows (m) {sapply(1:28, function (row) {  sum(sapply(1:28, function (col) { m[[row,col]] } )) })}
-
-function sumMatrixColumns(m) {
-	sapply(1:28, function (col) {  sum(sapply(1:28, function (row) { m[[row,col]] } )) })
-}
